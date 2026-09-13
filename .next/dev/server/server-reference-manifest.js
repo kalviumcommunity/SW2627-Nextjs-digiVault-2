93634bb7a302214f5f3408a9ef69a8a9089d9505
Chakshu-Bamotra-04 <chakshubamotra123@gmail.com>
2026-09-13 23:46:24 +0530

    Stop tracking Next.js build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WSS091nAX70VIxrFfRtNbpa1CFiwKQQ+wNLs8iTrMXY=\"\n}"
--- .next/dev/static/chunks/_1cxq3bf._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/_1joxapd._.js","static/chunks/src_042jd_a._.js","static/chunks/node_modules_1syo50v._.js"],
-    source: "entry"
-});
